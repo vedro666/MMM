6e07529d9508ac883fa7f4960f5b689c0354bbbe
Abdumalik <asoliyev53@gmail.com>
2025-11-03 17:50:52 +0500

    I'm Soliyev

--- functions.py
+++ functions.py
@@ -1,2 +1,4 @@
 def c2(x,y):
     return ((x**2 + y**2)**0.5)
+def func_soliyev(x,y):
+    return x*y
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from functions import c2
+from functions import c2, func_soliyev
 print(c2(9,16))
+print(func_soliyev(3,4))
